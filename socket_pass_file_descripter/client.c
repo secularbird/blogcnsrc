7503890cb77a98c0b007e4dd22942020146e5dda
zhuangyao <secularbird.eagle@gmail.com>
2014-01-25 19:47:12 +0800

    add socket pass file descripter samples

---
+++ socket_pass_file_descripter/client.c
@@ -0,0 +1,98 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/un.h>
+#include <unistd.h>
+#include <limits.h>
+
+int recv_fd(int socket)
+{
+	int sent_fd, available_ancillary_element_buffer_space;
+	struct msghdr socket_message;
+	struct iovec io_vector[1];
+	struct cmsghdr *control_message = NULL;
+	char message_buffer[1];
+	char ancillary_element_buffer[CMSG_SPACE(sizeof(int))];
+
+	/* start clean */
+	memset(&socket_message, 0, sizeof(struct msghdr));
+	memset(ancillary_element_buffer, 0, CMSG_SPACE(sizeof(int)));
+
+	/* setup a place to fill in message contents */
+	io_vector[0].iov_base = message_buffer;
+	io_vector[0].iov_len = 1;
+	socket_message.msg_iov = io_vector;
+	socket_message.msg_iovlen = 1;
+
+	/* provide space for the ancillary data */
+	socket_message.msg_control = ancillary_element_buffer;
+	socket_message.msg_controllen = CMSG_SPACE(sizeof(int));
+
+	if(recvmsg(socket, &socket_message, 0) < 0)
+		return -1;
+
+	if(message_buffer[0] != 'F')
+	{
+		/* this did not originate from the above function */
+		return -1;
+	}
+
+	if((socket_message.msg_flags & MSG_CTRUNC) == MSG_CTRUNC)
+	{
+		/* we did not provide enough space for the ancillary element array */
+		return -1;
+	}
+
+	/* iterate ancillary elements */
+	for(control_message = CMSG_FIRSTHDR(&socket_message);
+	control_message != NULL;
+	control_message = CMSG_NXTHDR(&socket_message, control_message))
+	{
+		if( (control_message->cmsg_level == SOL_SOCKET) &&
+			(control_message->cmsg_type == SCM_RIGHTS) )
+		{
+			sent_fd = *((int *) CMSG_DATA(control_message));
+			return sent_fd;
+		}
+	}
+
+	return -1;
+}
+
+int main (int argc, char const *argv[])
+{
+	struct sockaddr_un server_address;
+	int socket_fd;
+	int rev_fd = -1;
+	char *buffer = "This is from client";
+	
+	socket_fd = socket(AF_UNIX, SOCK_STREAM, 0);
+	if(socket_fd < 0)
+	{
+		perror("socket() failed\n");
+		return 1;
+	}
+
+	/* start with a clean address structure */
+	memset(&server_address, 0, sizeof(struct sockaddr_un));
+ 
+	server_address.sun_family = AF_UNIX;
+	snprintf(server_address.sun_path, sizeof(server_address.sun_path), "./demo_socket");
+
+	if(connect(socket_fd, (struct sockaddr *) &server_address, sizeof(struct sockaddr_un)) != 0)
+	{
+		perror("connect() failed\n");
+		return 1;
+	} 
+	
+	
+	rev_fd = recv_fd(socket_fd);
+	printf("recevied fd is %d\n",rev_fd);
+	sleep(10);
+	write(rev_fd, buffer, strlen(buffer)+1);
+	
+	close(socket_fd);
+	
+	return 0;
+}
---
+++ socket_pass_file_descripter/server.c
@@ -0,0 +1,94 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <errno.h>
+#include <string.h>
+#include <unistd.h>
+#include <limits.h>
+
+int send_fd(int socket, int fd_to_send)
+{
+	struct msghdr socket_message;
+	struct iovec io_vector[1];
+	struct cmsghdr *control_message = NULL;
+	char message_buffer[1];
+	/* storage space needed for an ancillary element with a paylod of length is CMSG_SPACE(sizeof(length)) */
+	char ancillary_element_buffer[CMSG_SPACE(sizeof(int))];
+	int available_ancillary_element_buffer_space;
+
+	/* at least one vector of one byte must be sent */
+	message_buffer[0] = 'F';
+	io_vector[0].iov_base = message_buffer;
+	io_vector[0].iov_len = 1;
+
+	/* initialize socket message */
+	memset(&socket_message, 0, sizeof(struct msghdr));
+	socket_message.msg_iov = io_vector;
+	socket_message.msg_iovlen = 1;
+
+	/* provide space for the ancillary data */
+	available_ancillary_element_buffer_space = CMSG_SPACE(sizeof(int));
+	memset(ancillary_element_buffer, 0, available_ancillary_element_buffer_space);
+	socket_message.msg_control = ancillary_element_buffer;
+	socket_message.msg_controllen = available_ancillary_element_buffer_space;
+
+	/* initialize a single ancillary data element for fd passing */
+	control_message = CMSG_FIRSTHDR(&socket_message);
+	control_message->cmsg_level = SOL_SOCKET;
+	control_message->cmsg_type = SCM_RIGHTS;
+	control_message->cmsg_len = CMSG_LEN(sizeof(int));
+	*((int *) CMSG_DATA(control_message)) = fd_to_send;
+
+	return sendmsg(socket, &socket_message, 0);
+}
+
+
+int main (int argc, char const *argv[])
+{
+	struct sockaddr_un client_address;
+	struct sockaddr_un server_address;
+	int socket_fd = 0;
+	int client_fd = 0;
+	int address_length = 0;
+	
+	socket_fd = socket(AF_UNIX, SOCK_STREAM, 0);
+	if(socket_fd < 0) {
+		perror("socket() failed\n");
+	}
+	
+	unlink("./demo_socket");
+	
+   	/* start with a clean address structure */
+    memset(&server_address, 0, sizeof(struct sockaddr_un));
+	
+    server_address.sun_family = AF_UNIX;
+    snprintf(server_address.sun_path, sizeof(server_address.sun_path), "./demo_socket");
+	
+    if(bind(socket_fd, 
+            (struct sockaddr *) &server_address, 
+            sizeof(struct sockaddr_un)) != 0)
+    {
+     perror("bind() failed\n");
+     return 1;
+    }
+	
+    if(listen(socket_fd, 5) != 0)
+    {
+     perror("listen() failed\n");
+     return 1;
+    }
+	
+	if((client_fd = accept(socket_fd, (struct sockaddr *) &client_address, &address_length)) > -1 )
+	{
+		printf("clent connected\n");
+		if(send_fd(client_fd, 1) == 0) {
+			printf("fd sended\n");
+		} else {
+			perror("send failed\n");
+		}
+	}
+	
+	close(socket_fd);  
+	
+	return 0;
+}
